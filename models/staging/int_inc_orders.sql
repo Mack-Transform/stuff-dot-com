1eaea4af589890f0a543e0f6e0e034d6c9b4ddfd
Antoine Mack <antoine.mack@fishtownanalytics.com>
2022-10-25 13:33:49 +0000

    changes to yml regarding source freshness

--- models/staging/int_inc_orders.sql
+++ models/staging/int_inc_orders.sql
@@ -6,7 +6,7 @@ reps as (
 )
 
 select category, city, customer_segment, department, item, order_date, 
-        postal_code, o.region, rep, "STATE", emp_id, first_name, last_name
+        postal_code, o.region, rep, "STATE", emp_id, first_name, last_name, customer_id
 
 from orders o 
 left join reps r
