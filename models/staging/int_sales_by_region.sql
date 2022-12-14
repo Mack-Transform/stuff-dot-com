cb4adc52647d815d77d1200e1ebaaee92be1bf56
Antoine Mack <antoine.mack@fishtownanalytics.com>
2022-12-14 16:56:07 +0000

    create sales by region

---
+++ models/staging/int_sales_by_region.sql
@@ -0,0 +1,10 @@
+    with inc_orders as (
+   select * from {{ ref('int_inc_orders')}}
+)
+ 
+select customer_segment, department, region, count(customer_id) as order_count
+ 
+from inc_orders
+group by customer_segment, department, region
+ 
+order by region asc
