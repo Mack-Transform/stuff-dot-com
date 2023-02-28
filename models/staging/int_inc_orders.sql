30af13af9277e16f2c3f1050e40a2b2366c895aa
Antoine Mack <antoine.mack@fishtownanalytics.com>
2023-02-28 17:39:50 +0000

    removed col

--- models/staging/int_inc_orders.sql
+++ models/staging/int_inc_orders.sql
@@ -5,7 +5,7 @@ reps as (
     select * from {{ ref('stg_employee')}}
 )
 
-select category, city, customer_segment, department, item, order_date, 
+select category, city, customer_segment, item, order_date, 
         postal_code, o.region, rep, "STATE", emp_id, first_name, last_name, customer_id
 
 from orders o 
