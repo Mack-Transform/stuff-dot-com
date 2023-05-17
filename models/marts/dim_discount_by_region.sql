b3cdb00f48a9365b6b427e5de1ac8e7309507cd5
Antoine <108777283+Mack-Transform@users.noreply.github.com>
2023-05-17 11:24:19 -0500

    Marketing (#59)
    * del regions
    
    * added discount model for sales team
    
    ---------
    
    Co-authored-by: Antoine Mack <antoine.mack@fishtownanalytics.com>

---
+++ models/marts/dim_discount_by_region.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized = 'table',
+        tags=["sales"]
+    )
+}}
+
+with orders as (
+    select * from {{ ref('int_inc_orders')}}
+),
+discounts as (
+    select * from {{ ref('dim_sales_by_product')}}
+)
+
+select city, "STATE", customer_segment, o.item, o.item_id, o.order_date,
+        postal_code, region, rep,  discount, product_base_margin
+from orders o
+left join 
+discounts d 
+on o.item_id = d.item_id
